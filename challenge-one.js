af67943d30ca7604d970855ecfdd6cab43aa19d4
Abdelaziz-ABDELHAK <abdelaziz.abdelhak03@gmail.com>
2024-12-22 11:10:07 +0100

    update

--- challenge-one.js
+++ challenge-one.js
@@ -8,14 +8,12 @@ console.log(`This message contains ${wordCount} words`);
 
 // Here is a functions that does the same job :
 
-function countWords(text) {
+function wordCount(text) {
   let words = text.split(" ").filter((word) => word.trim() !== "");
   return words.length;
 }
 
 let text = "There is no war in Ba Sing Se";
-let wordCount = countWords(text);
-
-console.log(`This text contains ${wordCount} words`);
+console.log(`This text contains ${wordCount(text)} words`);
 
 //**Run these two code blocks seperately**
